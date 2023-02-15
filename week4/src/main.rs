43767a0d51774adeccdd12c95bc675c1b21ef0b8
Marco Romano <marcojulioromano@gmail.com>
2023-02-15 17:42:57 +0100

    Week 4
    Statistics

--- week4/src/main.rs
+++ week4/src/main.rs
@@ -1,5 +1,28 @@
+use reqwest::Body;
+use serde::Deserialize;
+
 fn main() {
-    let holidays = get_holidays_for_country_and_year("NO", 2023);
+    let countries = vec!["SE", "NO", "IT", "CA"];
+
+    for country in countries {
+        let holidays = get_holidays_for_country_and_year(country, 2023);
+        println!("Length {}", holidays.len())
+    }
 }
 
-fn get_holidays_for_country_and_year(country_code: &str, year: i32) -> String {}
+fn get_holidays_for_country_and_year(country_code: &str, year: i32) -> Vec<Holiday> {
+    reqwest::blocking::get(format!(
+        "https://date.nager.at/api/v3/publicholidays/{}/{}",
+        year, country_code
+    ))
+    .unwrap()
+    .json()
+    .unwrap()
+}
+
+#[derive(Debug, Deserialize)]
+struct Holiday {
+    date: String,
+    name: String,
+    localName: String,
+}
