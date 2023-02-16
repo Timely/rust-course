6260760e9e8fcefb30d846362c5ceeb703a3fb35
Marco Romano <marcojulioromano@gmail.com>
2023-02-16 15:39:13 +0100

    Print holidays for at least 2 countries

--- week4/src/main.rs
+++ week4/src/main.rs
@@ -1,12 +1,18 @@
 use chrono::{Datelike, NaiveDate};
 use serde::Deserialize;
+use std::collections::HashMap;
 
 fn main() {
     let countries = vec!["SE", "NO", "IT", "CA"];
-
+    let mut holidays_for_country = HashMap::new();
     for country in countries {
-        print!("Country code: {} ", country);
         let holidays = get_holidays_for_country_and_year(country, 2023);
+        holidays_for_country.insert(country, holidays);
+    }
+
+    for entry in &holidays_for_country {
+        print!("Country code: {} ", entry.0);
+        let holidays = entry.1;
         print!("Number of holidays: {} - ", holidays.len());
 
         for q in 1..=4 {
@@ -27,6 +33,24 @@ fn main() {
         }
         println!()
     }
+
+    let dates: Vec<NaiveDate> = holidays_for_country
+        .into_values()
+        .flatten()
+        .map(|h| h.date)
+        .collect();
+
+    let mut all_holy = HashMap::new();
+    for date in dates {
+        let count = all_holy.entry(date).or_insert(0);
+        *count += 1
+    }
+    let at_least_twice: Vec<(&NaiveDate, &i32)> = all_holy.iter().filter(|e| e.1 >= &2).collect();
+    println!("Days that are holiday in at least 2 countries: ");
+    for i in at_least_twice {
+        print!("{} is holiday {} times | ", i.0, i.1);
+    }
+    println!();
 }
 
 fn get_holidays_for_country_and_year(country_code: &str, year: i32) -> Vec<Holiday> {
