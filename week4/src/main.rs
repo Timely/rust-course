a92169b1ae3d8361cc8c153afb3a04ae550fe371
Marco Romano <marcojulioromano@gmail.com>
2023-02-16 15:04:56 +0100

    Print number of holidaysper quarter for each country

--- week4/src/main.rs
+++ week4/src/main.rs
@@ -1,12 +1,31 @@
-use reqwest::Body;
+use chrono::{Datelike, NaiveDate};
 use serde::Deserialize;
 
 fn main() {
     let countries = vec!["SE", "NO", "IT", "CA"];
 
     for country in countries {
+        print!("Country code: {} ", country);
         let holidays = get_holidays_for_country_and_year(country, 2023);
-        println!("Length {}", holidays.len())
+        print!("Number of holidays: {} - ", holidays.len());
+
+        for q in 1..=4 {
+            let quarter: Vec<u32> = holidays
+                .iter()
+                .map(|h| h.date.month())
+                .filter(|m| {
+                    let min_bound = (q - 1) * 3;
+                    let max_bound = min_bound + 3;
+                    if m > &min_bound && m <= &max_bound {
+                        true
+                    } else {
+                        false
+                    }
+                })
+                .collect();
+            print!("Q{}: {}; ", q, quarter.len());
+        }
+        println!()
     }
 }
 
@@ -22,7 +41,7 @@ fn get_holidays_for_country_and_year(country_code: &str, year: i32) -> Vec<Holid
 
 #[derive(Debug, Deserialize)]
 struct Holiday {
-    date: String,
+    date: NaiveDate,
     name: String,
     localName: String,
 }
